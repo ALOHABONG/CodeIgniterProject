574059cdf7449856cf5bab4b1cc76d64be79d9f2
ALOHABONG <chrisjin13@naver.com>
2018-11-19 21:01:43 +0900

    topic_list bootstrap

--- application/models/topic_model.php
+++ application/models/topic_model.php
@@ -1,16 +1,20 @@
 <?php
-class Topic_model extends CI_MODEL {
-    function __construct(){
+class Topic_model extends CI_MODEL
+{
+    public function __construct()
+    {
         parent::__construct();
     }
 
-    public function get($topic_id){
+    public function get($topic_id)
+    {
         return $this->db->get_where('topic', array('id'=>$topic_id))->row();
         //return $this->db->query('SELECT * FROM topic WHRE id='.$topic_id)
     }
 
-    public function gets(){
-        $this->db->select('title');
+    public function gets()
+    {
+        //$this->db->select('title');
         return $this->db->query('SELECT * FROM topic')->result();
     }
 }
